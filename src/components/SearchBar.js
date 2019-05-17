bf18a53afb813eed63a67b5959cf37b7b16b0b12
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-05-17 10:28:27 -0300

    add event handlers to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })} />
           </div>
         </form>
       </div>
