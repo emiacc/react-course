8f77644fa7bcc62b79e5a95bbd13277694a55357
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-05-31 16:37:15 -0300

    dispatch actions

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
   renderList() {
@@ -7,7 +8,8 @@ class SongList extends React.Component {
       return(
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button className="ui button primary"
+              onClick={() => this.props.selectSong(song) }>
               Select   
             </button>
           </div> 
@@ -26,4 +28,4 @@ const mapStateToProps = state => {
   return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
