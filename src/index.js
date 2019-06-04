4e296bf15062d2b4e152c68d186a847676d56df2
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-06-03 23:13:55 -0300

    apply middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}> <App /> </Provider>,
+  <Provider store={store}> <App /> </Provider>,
   document.querySelector('#root')
 );
