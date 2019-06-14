5481ad43b4182e2c9b20198624b645098e3713b2
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-06-14 18:44:42 -0300

    router set up and components creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
 
-const App = () => <div>Hello World</div>;
+const App = () => {
+  return(
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+};
 
 export default App;
