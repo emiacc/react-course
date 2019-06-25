6c4eb5803699526ea6ca005e12a07dba4df1fa92
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-06-25 16:31:34 -0300

    api setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,14 @@
 import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../apis/streams';
 
 export const signIn = userId => { 
   return { type: SIGN_IN, payload: userId }; 
 };
 
-export const signOut = () => { return { type: SIGN_OUT }; };
+export const signOut = () => { 
+  return { type: SIGN_OUT }; 
+};
+
+export const createStream = formValues => async dispatch => {
+  streams.post('/streams', formValues);
+};
--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component{
   renderInput = ({ input, label, meta }) => {
@@ -23,8 +25,8 @@ class StreamCreate extends React.Component{
     }
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -52,7 +54,10 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+
+export default connect(null, { createStream })(formWrapped);
