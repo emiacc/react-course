6125be9ae2c34de26e5f0a72f08e88ca8ef451da
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-06-26 14:50:26 -0300

    buttons when user is logged in

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,9 +7,47 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
-  render() {
-    return <div>StreamList</div>;
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
   }
-} 
 
-export default connect(null, { fetchStreams })(StreamList);
+  renderList() {
+    return this.props.streams.map(stream => {
+      return(
+        <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });  
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { 
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId 
+  };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
