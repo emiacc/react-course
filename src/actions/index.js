4828be597925a1f716026d5deab23c54d9419d95
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-06-30 20:39:32 -0300

    programmatic navigation

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,7 @@ import {
   DELETE_STREAM,
   EDIT_STREAM
 } from '../actions/types';
+import history from '../history';
 
 export const signIn = userId => { 
   return { type: SIGN_IN, payload: userId }; 
@@ -20,6 +21,7 @@ export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', { ...formValues, userId});
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
