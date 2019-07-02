daa485ddedb27f82719b768e37b8bf6751ae9a54
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-07-02 12:24:14 -0300

    parameterizable modal refactor

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,6 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchStream, deleteStream } from '../../actions'; 
 import Modal from '../Modal';
+import history from '../../history';
 
-const StreamDelete = () => <Modal />;
+class StreamDelete extends React.Component { 
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-export default StreamDelete;
+  renderActions() {
+    return (
+      <React.Fragment>  
+        <button onClick={() => this.props.deleteStream(this.props.match.params.id)} className="ui button negative">Delete</button>
+        <Link to="/" className="ui button">Cancel</Link>
+      </React.Fragment>
+    );
+  }
+  
+  renderContent() {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete this stream?';
+    }
+
+    return `Are you sure you want to delete ${this.props.stream.title}?`;
+  }
+
+  render() {
+    return <Modal title="Delete Stream"
+              content={this.renderContent()}
+              actions={this.renderActions()}
+              onDismiss={() => history.push('/')} />;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
