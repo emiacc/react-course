ff618934ccd79063a264883397ea52f43a91a51b
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-07-05 15:42:47 -0300

    context provider

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === 'english' ? 'Submit' : 'Enviar';
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
