8b3bdd38a0f370430db569a4fea595d05adde921
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-07-05 15:15:31 -0300

    components creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,25 @@
 import React from 'react'
+import UserCreate from './UserCreate';
 
-const App = () => <div>Hello World</div>;
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+  
+  render() {
+    return(
+      <div className="ui container">
+        <div>
+          Select a language: 
+          <i className="flag uk" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag es" onClick={() => this.onLanguageChange('spanish')} />
+        </div>
+        <UserCreate />
+      </div>  
+    );
+  }
+} 
 
 export default App;
