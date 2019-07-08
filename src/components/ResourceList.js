fcbdab623fd4b544403418b7c4bf6836fa4e631c
Emiliano Scavuzzo <emiacc@hotmail.com>
2019-07-08 14:49:50 -0300

    useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-  state = { resources: [] };
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
 
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-    this.setState({ resources: response.data });
+  const fetchResources = async resource => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+    setResources(response.data);
   }
 
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-      this.setState({ resources: response.data });
-    }
-  }
+  useEffect(() => {
+    fetchResources(resource);
+  }, [resource]);
 
-  render() {
-    return <div>{this.state.resources.length}</div>;
-  }
-}
+  return(
+    <ul>
+      { resources.map(record => (
+        <li key={record.id}>{record.title}</li> 
+      ))}
+    </ul>
+  );
+};
 
 export default ResourceList;
